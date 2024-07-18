570c987ca74d51077f16c900abd89c5c89e650d7
zueunjoeng <judms0314@gmail.com>
2024-07-18 13:18:14 +0900

    useEffect 실행식(if 추가), 클래스에 삼항식

--- src/Qna.js
+++ src/Qna.js
@@ -1,49 +1,33 @@
-import React, { Fragment, useEffect } from 'react'
-import qnalist from './json/qna.json'
+import React, { Fragment, useEffect } from 'react';
+import qnalist from './json/qna.json';
 
-const {qna} = qnalist;
-//구조할당, 같은 표현 > const {qna} = qna.qna;
+const { qna } = qnalist;
 
-function Qna(props) {  
+function Qna(props) {
 
+    const dtsclick = (num) => {
         const dts = document.querySelectorAll("#qna dt");
-        //대상을 저장하는 것은 useEffect에 넣지 않아도 실행
-        //실행식은 useEffect안에 넣어줘야 한다.
-
-        // dts[0].addEventListener('click', ()=>{
-        //     dts[0].classList.add('on');
-        // })
-        // dts[0].click();
-
-        const dtsclick = (num)=>{
-            dts.forEach((v, i)=>{
+        if (dts[num]) {
+            dts.forEach((v, i) => {
                 v.classList.remove('on');
-
-            })
-            dts[num].classList.add('on')
+            });
+            dts[num].classList.add('on');
         }
-
-        useEffect(()=>{   
-            dtsclick(0) //실행식
-           }, [])
-        
+    };
+// classList 를 읽지못하는 오류는 -> if로 돌려야한다.
 
     return (
-      
-    <dl className='container my-5' id={props.keynm}>
-        {
-         qna.map((v, i)=>{
-                return(
+        <dl className='container my-5' id={props.keynm}>
+            {
+                qna.map((v, i) => (
                     <Fragment key={`qnalist${i}`}>
-                        <dt className='bg-dark text-white' onClick={()=>{dtsclick(i)}}>{v.Q}</dt>
+                        <dt className={`bg-dark text-white ${i === 0 ? 'on' : null}`} onClick={() => dtsclick(i)}>{v.Q}</dt>
                         <dd className='py-5'>{v.A}</dd>
                     </Fragment>
-                )
-            })
-        } 
-    </dl>
-    )
-   
+                ))
+            }
+        </dl>
+    );
 }
 
-export default Qna
+export default Qna;
