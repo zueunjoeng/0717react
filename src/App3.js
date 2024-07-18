be7eb54762ad7085a6d92fcfdf5e9415158d03dc
zueunjoeng <judms0314@gmail.com>
2024-07-18 11:10:58 +0900

    삼항식

--- src/App3.js
+++ src/App3.js
@@ -1,42 +1,40 @@
 import React, { useState }from 'react'
-import tabjson from "./json/tab.json"
+import tabjson from './json/tab.json'
 
+function App3() {  
+    const [tabnm , setTabnm ] = useState('notice');
 
-function App3() {
-    const [tabnm, setTabnm] = useState('notice');
-     return (
+    return (
         <div className="tabwrap d-flex container border">
             <ul className="tabs border">
-                <li><button onClick={()=>{setTabnm('notice')}}>{tabjson["notice"]["title"]}</button></li>
-                <li><button onClick={()=>{setTabnm('gallery')}}>{tabjson["gallery"]["title"]}</button></li>
-                <li><button onClick={()=>{setTabnm('qna')}}>{tabjson["qna"]["title"]}</button></li>
+                <li><button onClick={()=>{ setTabnm('notice') }} >{tabjson["notice"]["title"]}</button></li>
+                <li><button onClick={()=>{ setTabnm('gallery') }} >{tabjson["gallery"]["title"]}</button></li>
+                <li><button onClick={()=>{ setTabnm('qna') }} >{tabjson["qna"]["title"]}</button></li>
             </ul>
             <div className="content col border">
                 <ul>
                 {
-                    tabjson[tabnm]["content"].map((v, i)=>{
-                        return(
-                            <li key={`list${i}`}>
-                                {
-                                    v.split("|").map((vv, ii)=>{
-                                     return(
-                                        <>
-                                       ii === 0 && <h2>{vv}</h2> && 10
-                                     {/* &연산자 - 끝까지 읽고 랜더링되는 친구 /  마지막 && 10은 앞에 친구들이 참일때  */}
-                                        </>
-                                     )
-                                    })
-                                }
-                            </li>
-                        )
-                    })
+                  tabjson[tabnm]["content"].map(( v, i )=>{
+                    return(
+                        <li key={`list${i}`} >
+                            {
+                                v.split("|").map((vv, ii)=>{
+                                    return(
+                                      
+                                     ii === 0 ? <h2>{vv}</h2> : <p>{vv}</p>
+                                      
+                                     
+                                    )
+                                })
+                            }
+                        </li>
+                    )
+                 })
                 }
                 </ul>
             </div>
-        </div>
-        
+        </div> 
     )
 }
 
 export default App3
-
